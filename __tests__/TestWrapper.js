b76b6432b3aea11ddb91149d7eeae8091d4b5b7e
Matthias <git@myrho.net>
2018-02-20 12:27:23 +0100

    correct async testing

--- __tests__/TestWrapper.js
+++ __tests__/TestWrapper.js
@@ -1,22 +1,28 @@
 import Bright from '../src/Bright.js'
+import Logger from 'logplease'
 
-test('ping yields pong', () => {
+let logger = Logger.create('TestWrapper')
+
+test('ping yields pong', done => {
   let bright = new Bright()
   let origin = 'X'
 
   bright.on('message', (target, message) => {
     expect(target).toEqual(origin)
     expect(message).toEqual({type: 'pong'})
+    done()
   })
 
   bright.message(origin, {type : 'ping'})
 })
 
-test('register', () => {
+test('register', done => {
   let bright = new Bright()
   let origin = 'X'
 
   bright.on('message', (target, message) => {
+    expect(message).toEqual({type: 'registered'})
+    done()
   })
 
   bright.message(origin, {type : 'register', uri : "localhost/bob"})
