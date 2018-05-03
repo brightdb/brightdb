c0ccebc0eefe92b7ec845684cb437e8ce6c775dc
Matthias <git@myrho.net>
2018-05-03 11:03:58 +0200

    add turn servers

--- src/BrightSimplePeer.js
+++ src/BrightSimplePeer.js
@@ -8,6 +8,22 @@ const eventExists = (e) => {
   return events.indexOf(e) !== -1
 }
 
+const iceServers = [
+  { 
+    url:'stun:stun.l.google.com:19302'
+  },
+  {
+      url: 'turn:192.158.29.39:3478?transport=udp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+  },
+  {
+      url: 'turn:192.158.29.39:3478?transport=tcp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+  }
+]
+
 export default function BrightSimplePeer(WRTC) {
   let peers = {}
   let handlers = {}
